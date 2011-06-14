05647dd9c289a21efccac7e93f76173e69493268
Maurits van Rees <maurits@vanrees.org>
2011-06-14 12:54:00 +0000

    Moved all options to a new config.py file.
    svn path=/poi.receivemail/trunk/; revision=240875

---
+++ poi/receivemail/config.py
@@ -0,0 +1,15 @@
+# We only listen on these addresses.  Or actually, we only do a few
+# special tricks to switch the user and give him more priviliges when
+# the request comes from one of these addresses.  Make this an empty
+# list or tuple if you do not want those special tricks at all.
+LISTEN_ADDRESSES = ('127.0.0.1', )
+
+# Should we fake a Manager role to be sure that a post succeeds?
+FAKE_MANAGER = True
+
+# Do advanced matching on subject to find existing issue that this
+# email could be a reply to?  When False we just look for '#123' in
+# the Subject line.  When True we search for issues with a Title
+# matching the Subject line as well.
+ADVANCED_SUBJECT_MATCH = False
+
--- poi/receivemail/poimail.py
+++ poi/receivemail/poimail.py
@@ -23,21 +23,11 @@ from Products.CMFPlone.utils import _createObjectByType
 from Products.Poi.adapters import IResponseContainer
 from Products.Poi.adapters import Response
 
+from poi.receivemail.config import LISTEN_ADDRESSES
+from poi.receivemail.config import FAKE_MANAGER
+from poi.receivemail.config import ADVANCED_SUBJECT_MATCH
+
 logger = logging.getLogger('poimail')
-
-# We only listen on these addresses.  Or actually, we only do a few
-# special tricks to switch the user and give him more priviliges when
-# the request comes from one of these addresses.  Make this an empty
-# list or tuple if you do not want those special tricks at all.
-LISTEN_ADDRESSES = ('127.0.0.1', )
-
-# Should we fake a Manager role to be sure that a post succeeds?
-FAKE_MANAGER = True
-# Do advanced matching on subject to find existing issue that this
-# email could be a reply to?  When False we just look for '#123' in
-# the Subject line.  When True we search for issues with a Title
-# matching the Subject line as well.
-ADVANCED_SUBJECT_MATCH = False
 
 
 def cleanup_search_string(s):
