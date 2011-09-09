e7b09228b1883f8a00224678d7ab7ced53081a49
Maurits van Rees <maurits@vanrees.org>
2011-09-09 17:51:04 +0000

    Renamed docs/HISTORY.txt to CHANGES.txt
    svn path=/poi.receivemail/trunk/; revision=244327

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.7dev'
 
@@ -7,7 +6,7 @@ setup(name='poi.receivemail',
       version=version,
       description="Receive email in the Poi issue tracker",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
