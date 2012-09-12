a0db9a5fad5e116400af7ff332f9bab11ca44f99
Maurits van Rees <maurits@vanrees.org>
2012-09-13 01:10:31 +0200

    Renamed CHANGES.txt to CHANGES.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='poi.receivemail',
       version=version,
       description="Receive email in the Poi issue tracker",
       long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
+                       open("CHANGES.rst").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
