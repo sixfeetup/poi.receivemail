a6b59a7deb62f9c4b3ea12281c8a1c9352d16b5b
Maurits van Rees <maurits@vanrees.org>
2012-10-14 02:36:18 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ version = '1.11dev'
 setup(name='poi.receivemail',
       version=version,
       description="Receive email in the Poi issue tracker",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.rst").read(),
+      long_description=(open("README.txt").read().strip() + "\\nn" +
+                        open("CHANGES.rst").read().strip()),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.2",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.2",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Programming Language :: Python",
+          ],
       keywords='',
       author='Maurits van Rees',
       author_email='maurits@vanrees.org',
